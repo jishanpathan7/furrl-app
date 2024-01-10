969b64a8286910af58caf9e1725f6ea84e5ec8e2
Jishan Pathan <88240594+jishanpathan7@users.noreply.github.com>
2024-01-10 16:29:18 +0530

    code changes

--- components/OrgChart.js
+++ components/OrgChart.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Tree, TreeNode } from "react-organizational-chart";
 import orgChartStyles from "./OrgChart.module.css";
 import Avatar from "@mui/material/Avatar";
+import { Edit } from "@mui/icons-material";
 
 const OrgChart = ({ data }) => {
   const renderNode = ({ node }) => (
@@ -15,11 +16,23 @@ const OrgChart = ({ data }) => {
             background: "#db9adb",
           }}
         />
-      
+        <Edit
+          className={orgChartStyles.editIcon}
+          style={{
+            width: "18px",
+            height: "18px",
+          }}
+        />
       </div>
       <div>
-        <div className={orgChartStyles.name}>{node.name}</div>
-        <div className={orgChartStyles.designation}>{node.designation}</div>
+        <div className={orgChartStyles.name}>
+          <span>{node.name}</span>
+        </div>
+
+        <div className={orgChartStyles.designation}>
+          <strong className={orgChartStyles.role}>ROLE :</strong>
+          {node.designation}
+        </div>
       </div>
     </div>
   );
@@ -54,7 +67,7 @@ const OrgChart = ({ data }) => {
       }
     >
       {/* {data.map((employee, index) => renderTree(employee, index))} */}
-      {data.map((employee, index) =>  renderTree(employee, index))}
+      {data.map((employee, index) => renderTree(employee, index))}
     </Tree>
   );
 };
